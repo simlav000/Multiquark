6cb51f166e21fa06ee4557194b7389369747e559
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-05-06 11:11:32 -0400

    ROOT primer files/tutorial files

---
+++ ROOT/tutorial/InteractivePlotting.cpp
@@ -0,0 +1,21 @@
+// A very useful interactive tool is the FitPanel, available for the classes
+// TGraphErrors and TH1F. Predefined fit functions can be selected from a 
+// drop-down menu, including "gaus", "expo", and "pol0" - "pol9". Additionally,
+// user-defined functions using the same syntax as for functions with parameters
+// are possible!
+//
+// After setting the initial parameters, a fit of the selected function to the 
+// data of the graph can be performed and the result displayed on the plot.
+// The fit panel has a number of control options to select the fit method,
+// fix or release individual parameters in the fit, to steer the level of output
+// printed on the console, or to extract and display additional information
+// like contour lines showing parameter correlations. 
+//
+// Once you are satisfied with your plot, you will probably want to save it.
+// You can search for the "Save as..." option from the menu line of the window.
+// It will pop up a file selector box to allow you to choose the format, file
+// name and target directory. 
+// There is one very noticeable feature here: you can store a plot as a ROOT 
+// macro! In this macro, you find the C++ representation of all methods and
+// classes involved in generating the plot. This is a valuable source of 
+// information for your own macros.
---
+++ ROOT/tutorial/MyFirstMacro.c
@@ -0,0 +1,11 @@
+// declaring type aliases, more useful for more complicated types
+using dubs = double;
+
+dubs test = 9.9;
+
+int MyFirstMacro() {
+    cout << test << endl;
+    return 1;
+}
+
+
