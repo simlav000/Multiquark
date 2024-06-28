6da32541abd0e3dd30f12a0a8bd14360b3bdde0d
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-06-28 16:10:59 -0400

    Back to inline definitions in header. Probably violates some convention but its also way more convenient to have all definitions found by a single #include in MakeHists.cxx to be immediately interpreted by cling

--- scripts/Cuts.h
+++ scripts/Cuts.h
@@ -1,87 +1,109 @@
-#ifndef CUTS_H
-#define CUTS_H
-
 #include <TCut.h>
+#include <iomanip>
+#include <string>
 
-// ##########################
-// # Cuts common to all V0s #
-// ##########################
-extern float V0chi2_high;
-extern TCut cut_on_V0chi2;
-extern TCut no_cut;
+/*                      *\
 
-// ##########################
-// #     Cuts for Kaons     #
-// ##########################
+  Cuts common to all V0s
+
+\*                      */
+
+inline float V0chi2_high = 15;
+inline TCut cut_on_V0chi2 = Form("chi2ndf < %f", V0chi2_high);
+inline TCut no_cut = "";
+
+/*                *\
+
+   Cuts for Kaons 
+
+\*                */
 
 // mm
-extern float KDeltaRxy_low;
-extern float KDeltaRxy_high;
+inline float KDeltaRxy_low = 4;
+inline float KDeltaRxy_high = 450;
 
 // MeV
-extern float KpT_low;
-extern float K_LmassBand1_low;
-extern float K_LmassBand1_high;
-extern float K_LmassBand2_low;
+inline float KpT_low = 300;
+inline float K_LmassBand1_low = 0;
+inline float K_LmassBand1_high = 1105;
+inline float K_LmassBand2_low = 1125;
 
-extern float KcosTheta_3D_low;
+inline float KcosTheta_3D_low = 0.9990;
 
-extern TCut cut_on_KDeltaRxy;
-extern TCut cut_on_KpT;
-extern TCut cut_on_KcosTheta_3D;
-extern TCut cut_on_K_LmassBand1;
-extern TCut cut_on_K_LmassBand2;
-extern TCut cut_on_K_Lmass;
+inline TCut cut_on_KDeltaRxy    = Form("DeltaR > %f && DeltaR < %f", KDeltaRxy_low, KDeltaRxy_high);
+inline TCut cut_on_KpT          = Form("p_T > %f", KpT_low);
+inline TCut cut_on_KcosTheta_3D = Form("CosTheta > %f", KcosTheta_3D_low);
+inline TCut cut_on_K_LmassBand1 = Form("LMass > %f && LMass < %f", K_LmassBand1_low, K_LmassBand1_high);
+inline TCut cut_on_K_LmassBand2 = Form("LMass > %f", K_LmassBand2_low);
+inline TCut cut_on_K_Lmass      = cut_on_K_LmassBand1 || cut_on_K_LmassBand2;
 
 // Kmass signal region: PDG(497.611) +- 20
-extern float Kmass_PDG;
-extern float Kmass_signal_low;    // PDG -20
-extern float Kmass_signal_high;    // PDG +20
+inline float Kmass_PDG         = 497.611;
+inline float Kmass_signal_low  = 477.611;    // PDG -20
+inline float Kmass_signal_high = 517.611;    // PDG +20
 
-extern TCut cut_on_Kmass_signal;
+inline TCut cut_on_Kmass_signal = Form("KMass > %f && KMass < %f", Kmass_signal_low, Kmass_signal_high);
 
-extern TCut KCut2;
-extern TCut KCut3;
-extern TCut cut_on_KcosTheta_and_pT;
-extern TCut K_candidate_cuts; // for plotting K mass distribution
-extern TCut K_signal_cuts; // for getting kaons in mass signal region
+inline TCut KCut2 = cut_on_KcosTheta_3D && cut_on_KpT;
+inline TCut KCut3 = KCut2 && cut_on_KDeltaRxy;
+inline TCut K_candidate_cuts = cut_on_V0chi2 && cut_on_KDeltaRxy && cut_on_KpT && cut_on_KcosTheta_3D && cut_on_K_Lmass; // for plotting K mass distribution
+inline TCut K_signal_cuts    = K_candidate_cuts && cut_on_Kmass_signal; // for getting kaons in mass signal region
 
-// ##########################
-// #    Cuts for Lambdas    #
-// ##########################
-extern float k;
+/*                *\
+
+  Cuts for Lambdas
+
+\*                */
 
 // All cuts except mass signal cuts are common to both L and LB
-extern float LDeltaRxy_low;
-extern float LDeltaRxy_high;
-extern float LpT_low;
-extern float LcosTheta_3D_low;
-extern float L_KmassBand1_low;
-extern float L_KmassBand1_high;
-extern float L_KmassBand2_low;
+inline float LDeltaRxy_low = 17;
+inline float LDeltaRxy_high = 450;
+inline float LpT_low = 500;
+inline float LcosTheta_3D_low = 0.9998;
+inline float L_KmassBand1_low = 340;
+inline float L_KmassBand1_high = 480;
+inline float L_KmassBand2_low = 520;
 
 
-extern TCut cut_on_LDeltaRxy;
-extern TCut cut_on_LpT;
-extern TCut cut_on_LcosTheta_3D;
-extern TCut cut_on_L_KmassBand1;
-extern TCut cut_on_L_KmassBand2;
-extern TCut cut_on_L_Kmass;
+inline TCut cut_on_LDeltaRxy    = Form("DeltaR > %f && DeltaR < %f", LDeltaRxy_low, LDeltaRxy_high);
+inline TCut cut_on_LpT          = Form("p_T > %f", LpT_low);
+inline TCut cut_on_LcosTheta_3D = Form("CosTheta > %f", LcosTheta_3D_low);
+inline TCut cut_on_L_KmassBand1 = Form("KMass > %f && KMass < %f", L_KmassBand1_low, L_KmassBand1_high);
+inline TCut cut_on_L_KmassBand2 = Form("KMass > %f", L_KmassBand2_low);
+inline TCut cut_on_L_Kmass      = cut_on_L_KmassBand1 || cut_on_L_KmassBand2;
 
 // Lmass signal region: PDG(1115.683) +- 7
-extern float Lmass_PDG;
-extern float Lmass_signal_low;    // PDG -7  
-extern float Lmass_signal_high;    // PDG +7
+inline float Lmass_PDG          = 1115.683;
+inline float Lmass_signal_low   = 1108.683;    // PDG -7  
+inline float Lmass_signal_high  = 1122.683;    // PDG +7
 
-extern TCut cut_on_Lmass_signal;   
-extern TCut cut_on_LBmass_signal;
+inline TCut cut_on_Lmass_signal = Form("LMass > %f && LMass < %f", Lmass_signal_low, Lmass_signal_high);   
+inline TCut cut_on_LBmass_signal = Form("LBarMass > %f && LBarMass < %f", Lmass_signal_low, Lmass_signal_high);
 
 
-extern TCut L_LB_candidate_cuts; // for plotting L or LB mass distribution
-extern TCut L_signal_cuts; // for getting lambdas in mass signal region
-extern TCut LB_signal_cuts; // for getting lambdabars in mass signal region
+inline TCut L_LB_candidate_cuts = cut_on_V0chi2 && cut_on_LDeltaRxy && cut_on_LpT && cut_on_LcosTheta_3D && cut_on_L_Kmass; // for plotting L or LB mass distribution
+inline TCut L_signal_cuts    = L_LB_candidate_cuts && cut_on_Lmass_signal; // for getting lambdas in mass signal region
+inline TCut LB_signal_cuts   = L_LB_candidate_cuts && cut_on_LBmass_signal; // for getting lambdabars in mass signal region
 
-extern std::string floatToString(float v, int p);
-extern void SetCutNames();
+/*                 *\
 
-#endif // CUTS_H
+  Setting Cut Names
+
+\*                 */
+
+// Utility to convert inline float to string with specific precision
+inline std::string floatToString(float v, int p) {
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(p) << v;
+    return ss.str();
+}
+
+inline void SetCutNames() {
+    // Used for automatically printing cut descriptions in histogram legend. 
+    no_cut.SetName("No cuts");
+    cut_on_KcosTheta_3D.SetName(("Cos(#theta) > " + floatToString(KcosTheta_3D_low, 3)).c_str());
+    K_candidate_cuts.SetName("All cuts");
+    KCut2.SetName(("p_T > " + floatToString(KpT_low, 2) + " MeV").c_str());
+    KCut3.SetName((floatToString(KDeltaRxy_low, 2) + " mm < R > " + floatToString(KDeltaRxy_high, 2) + " mm").c_str());
+
+}
