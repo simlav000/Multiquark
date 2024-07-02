577dd93ae411d405a3c7d15a3547ba64085a81a2
simlav000 <simon.lavoie@mail.mcgill.ca>
2024-07-02 14:00:31 -0400

    Class definition of particles, including Kaon and Lambda objects with default values

---
+++ scripts/Particles.h
@@ -0,0 +1,84 @@
+#include "Fits.h"
+
+#include <Rtypes.h>
+
+#include <string>
+
+class Particle {
+public:
+    std::string name;
+    std::string name_formatted;
+    
+    Double_t mass_pdg;
+
+    // Variables from myTree
+    std::string mass;
+    std::string life;
+
+    // Variables for plotting
+    Double_t mass_min;
+    Double_t mass_max;
+    Double_t life_min;
+    Double_t life_max;
+    Double_t (*mass_fit_model)(const Double_t*, const Double_t*);
+    Double_t (*life_fit_model)(const Double_t*, const Double_t*);
+
+    std::string invariant_mass_label;
+
+    // Constructor - Destructor
+    Particle(std::string n) : name(n) {}
+    ~Particle() {}
+
+};
+
+class Kaon : public Particle {
+public:
+    Kaon() : Particle("Kaon") {
+        this->name_formatted = "K^{0}_{s}";
+
+        this->mass_pdg = 497.611;
+
+        this->mass = "KMass";
+        this->life = "KLife";
+
+        // MeV
+        this->mass_min = 300;
+        this->mass_max = 650;
+
+        // s
+        this->life_min = 0.025e-9;
+        this->life_max = 0.22e-9;
+
+        this->mass_fit_model = KMassFit;
+        this->life_fit_model = lifetime_fit_2exp; 
+
+        this->invariant_mass_label = "m_{#pi^{+}#pi^{-}} [MeV]";
+    }
+    ~Kaon() {}
+};
+
+class Lambda : public Particle {
+public:
+    Lambda() : Particle("Lambda") {
+        this->name_formatted = "#Lambda^{0}";
+
+        this->mass_pdg = 1115.683;
+
+        this->mass = "LMass";
+        this->life = "LLife";
+
+        // MeV
+        this->mass_min = 1080;
+        this->mass_max = 1300;
+
+        // s
+        this->life_min = 0.025e-9;
+        this->life_max = 0.22e-9;
+
+        this->mass_fit_model = LMassFitBreitWigner;
+        this->life_fit_model = lifetime_fit_2exp; 
+
+        this->invariant_mass_label = "m_{p^{+}#pi^{-}} [MeV]";
+    }
+    ~Lambda() {}
+};
